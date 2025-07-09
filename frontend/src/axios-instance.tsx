963fa3290ceb1bbf71953dc8d9458b4897b7b0f8
Mansoor <mansoor_akhtar6@yahoo.com>
2025-07-09 19:34:04 +0500

    docker

--- frontend/src/axios-instance.tsx
+++ frontend/src/axios-instance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://localhost:9000/api/"
-  baseURL: "http://127.0.0.1:8000/api/"
+  baseURL: "http://localhost:9000/api/"
+  //baseURL: "http://127.0.0.1:8000/api/"
 });
